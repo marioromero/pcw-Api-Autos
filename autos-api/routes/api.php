9b23f6d995af2564b452d70f0ee94515e60da981
Mario Romero <marioromero.ttp@gmail.com>
2025-06-11 21:23:36 -0400

    Inicial

---
+++ autos-api/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+//ruta prueba que retorna un json de prueba con una lista de autos inventados
+Route::get('/cars', function () {
+    return response()->json([
+        ['id' => 1, 'make' => 'Toyota', 'model' => 'Corolla', 'year' => 2020],
+        ['id' => 2, 'make' => 'Honda', 'model' => 'Civic', 'year' => 2021],
+        ['id' => 3, 'make' => 'Ford', 'model' => 'Focus', 'year' => 2019],
+    ]);
+});
