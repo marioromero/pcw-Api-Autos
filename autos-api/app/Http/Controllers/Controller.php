6f907f2b6b66bc0e9cdd42f26c2adf1f98c55cd4
Mario Romero <marioromero.ttp@gmail.com>
2025-06-15 22:29:05 -0400

    Agrega soporte para devolver el código http desde el método de response, mediante el segundo parámetro del método json()

--- autos-api/app/Http/Controllers/Controller.php
+++ autos-api/app/Http/Controllers/Controller.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //método de respuesta que estandariza las respuestas de la API, mediante los atributos status, message y data
-    protected function response($status, $message, $data = null)
+    //método de respuesta que estandariza las respuestas de la API, mediante los atributos status, message y data, tambien
+    //permite definir un código de estado HTTP personalizado
+    protected function response(int $status, string $message, mixed $data = null, int $httpCode = 200)
     {
         return response()->json([
-            'status' => $status,
+            'status'  => $status,
             'message' => $message,
-            'data' => $data,
-        ]);
+            'data'    => $data,
+        ], $httpCode);
     }
 }
