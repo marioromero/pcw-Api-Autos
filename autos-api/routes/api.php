cc7f1b692a20bf0be46a48e2a232d38bead91700
Mario Romero <marioromero.ttp@gmail.com>
2025-06-15 22:32:13 -0400

    5 Rutas Ok

--- autos-api/routes/api.php
+++ autos-api/routes/api.php
@@ -3,11 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//ruta prueba que retorna un json de prueba con una lista de autos inventados
-Route::get('/cars', function () {
-    return response()->json([
-        ['id' => 1, 'make' => 'Toyota', 'model' => 'Corolla', 'year' => 2020],
-        ['id' => 2, 'make' => 'Honda', 'model' => 'Civic', 'year' => 2021],
-        ['id' => 3, 'make' => 'Ford', 'model' => 'Focus', 'year' => 2019],
-    ]);
-});
+//5 endpoints de la api, cada una apunta a un método del controlador CarController
+Route::get('/cars', 'App\Http\Controllers\CarController@index');
+Route::get('/cars/{id}', 'App\Http\Controllers\CarController@show');
+Route::post('/cars', 'App\Http\Controllers\CarController@store');
+Route::put('/cars/{id}', 'App\Http\Controllers\CarController@update');
+Route::delete('/cars/{id}', 'App\Http\Controllers\CarController@destroy');
+
+//Laravel provee un tipo de ruta "resource" que crea automáticamente las rutas para los métodos index, show, store, update y destroy
+//lo que sería equivalente a las 5 rutas anteriores, se puede llamar así:
+// Route::resource('cars', 'App\Http\Controllers\CarController');
