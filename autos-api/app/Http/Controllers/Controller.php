1f5d6a2bfb95bfd1dd84d2d0c33847fcd3349e89
Mario Romero <marioromero.ttp@gmail.com>
2025-06-15 20:23:26 -0400

    Creo método de respuesta en Controller.php para que los controladores que heredan, lo ocupen.

--- autos-api/app/Http/Controllers/Controller.php
+++ autos-api/app/Http/Controllers/Controller.php
@@ -4,5 +4,13 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    //método de respuesta que estandariza las respuestas de la API, mediante los atributos status, message y data
+    protected function response($status, $message, $data = null)
+    {
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data,
+        ]);
+    }
 }
--- autos-api/routes/api.php
+++ autos-api/routes/api.php
@@ -3,10 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 //ruta prueba que retorna un json de prueba con una lista de autos inventados
 Route::get('/cars', function () {
     return response()->json([
